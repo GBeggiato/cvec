cbc6b14098a820796de1f023776a341fe93b1815
GBegg <beggio.guido@gmail.com>
2025-08-20 10:43:47 +0200

    works with ints

--- cvec.c
+++ cvec.c
@@ -4,77 +4,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #define CV_INITIAL_CAP 32
 
-#define TYPE int
-#define TYPE_NAME Vec
-
-// --------------------------------------------------------------------
-//
-#define CV_CONCAT(a, b) a##b
-
-typedef struct {
-    size_t capacity;
-    size_t length;
-    TYPE *values;
-} TYPE_NAME;
-
-#define CV_NEW_DA(type, name) \
+#define CV_STRUCT(type, struct_name) \
 typedef struct { \
     size_t capacity; \
     size_t length; \
     type *values; \
-} (name) ; \
-\
-(name) CV_CONCAT(name, _new) (size_t item_size) { \
-    return (name){ \
+} (struct_name) ; \
+
+#define CV_NEW(type, struct_name) \
+struct_name struct_name##_new() { \
+    return (struct_name){ \
         .capacity = CV_INITIAL_CAP, \
         .length = 0, \
-        .values = malloc(CV_INITIAL_CAP * items_size) \
+        .values = malloc(CV_INITIAL_CAP * sizeof(type)) \
     }; \
 } \
 
-CV_NEW_DA(double, Nums)
+#define CV_REALLOC(struct_name) \
+void struct_name##_realloc(struct_name *v) { \
+    size_t new_cap = v->capacity == 0 ? CV_INITIAL_CAP : v->capacity * 2; \
+    v->values = realloc(v->values, new_cap * sizeof(*v->values)); \
+    assert(v->values != NULL); \
+    v->capacity = new_cap; \
+} \
 
-// TYPE_NAME vec_new(size_t items_size) {
-//     return (TYPE_NAME){
-//         .capacity = CV_INITIAL_CAP,
-//         .length = 0,
-//         .values = malloc(CV_INITIAL_CAP * items_size)
-//     };
-// }
-//
-// void vec_realloc(TYPE_NAME *v) {
-//     size_t new_cap = v->capacity * 2;
-//     v->values = realloc(v->values, new_cap * sizeof(*v->values));
-//     assert(v->values != NULL);
-//     v->capacity = new_cap;
-// }
-//
-// void vec_append(TYPE_NAME *v, TYPE n) {
-//     if (v->capacity == v->length) {
-//         vec_realloc(v);
-//     }
-//     v->values[v->length++] = n;
-// }
+#define CV_APPEND(type, struct_name) \
+void struct_name##_append(struct_name *v, type n) { \
+    if (v->capacity == v->length) struct_name##_realloc(v); \
+    v->values[v->length++] = n; \
+} \
 
-// #define VEC_NEW_OF(x) vec_new(sizeof(x))
-// Vec v = VEC_NEW_OF(TYPE);
 
-// --------------------------------------------------------------------
+#define CV_IMPL_DA(elem_type, struct_name) \
+CV_STRUCT(elem_type, struct_name) \
+CV_NEW(elem_type, struct_name) \
+CV_REALLOC(struct_name) \
+CV_APPEND(elem_type, struct_name) \
+
+
+CV_IMPL_DA(int, Ints)
+
 
 int main(void) {
-    // Vec v = vec_new(sizeof(TYPE));
-    //
-    // for (size_t i = 0; i < 6; ++i){
-    //     vec_append(&v, i);
-    //     printf("%4d, (%zu, %zu)\n", v.values[i], v.length, v.capacity);
-    // }
 
-    int CV_CONCAT(name, _new) = 4;
-    printf("%d\n", name_new);
+    Ints ints = Ints_new();
+    Ints_append(&ints, 10);
+    Ints_append(&ints, 20);
+    Ints_append(&ints, 30);
+    Ints_append(&ints, 40);
 
 
+    for (size_t i = 0; i < ints.length; ++i){
+        printf("%d\n", ints.values[i]);
+    }
 
     printf("\nend\n");
 }
+
